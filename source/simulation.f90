52f59c79b00eadcdab3a262b0948380e45efff21
Felix <leimenstoll.felix@gmail.com>
2026-05-02 16:03:36 +0200

    perf: reworked alterantive version of shear wave decay to perform collision in a loop over the cells instead of using whole-array operations to reduce global memory transfers

--- source/simulation.f90
+++ source/simulation.f90
@@ -315,10 +315,24 @@ contains
         real(FP), intent(inout) :: u_y(N_X, N_Y)
 
         ! temp
-        integer(int32) :: i
-        real(FP) :: u_squ(N_X, N_Y)
-        real(FP) :: c_dot_u(N_X, N_Y)
-        real(FP) :: f_eq(N_X, N_Y)
+        integer(int32) :: x, y, i
+        real(FP) :: f_1
+        real(FP) :: f_2
+        real(FP) :: f_3
+        real(FP) :: f_4
+        real(FP) :: f_5
+        real(FP) :: f_6
+        real(FP) :: f_7
+        real(FP) :: f_8
+        real(FP) :: f_9
+        real(FP) :: f_pulled(N_DIRS)
+        real(FP) :: rho_val
+        real(FP) :: u_x_val
+        real(FP) :: u_y_val
+        real(FP) :: u_squ
+        real(FP) :: c_dot_u
+        real(FP) :: f_eq_val
+        real(FP) :: f_next_val
 
         ! 1: ( 0,  0) = rest
         ! 2: ( 1,  0) = east
@@ -344,46 +358,61 @@ contains
         f_next(:, :, 8) = cshift(cshift(f(:, :, 8), shift=1, dim=1), shift=1, dim=2)
         f_next(:, :, 9) = cshift(cshift(f(:, :, 9), shift=-1, dim=1), shift=1, dim=2)
 
-        ! density and velocity from streamed f
-        rho(:, :) = f(:, :, 1) + f_next(:, :, 2) + f_next(:, :, 3) + f_next(:, :, 4) + &
-                    f_next(:, :, 5) + f_next(:, :, 6) + f_next(:, :, 7) + f_next(:, :, 8) + f_next(:, :, 9)
-        u_x(:, :) = f_next(:, :, 2) - f_next(:, :, 4) + f_next(:, :, 6) - &
-                    f_next(:, :, 7) - f_next(:, :, 8) + f_next(:, :, 9)
-        u_y(:, :) = f_next(:, :, 3) - f_next(:, :, 5) + f_next(:, :, 6) + &
-                    f_next(:, :, 7) - f_next(:, :, 8) - f_next(:, :, 9)
+        ! loop over rows and cols of all cells
+        do y = 1, N_Y
+            do x = 1, N_X
 
-        ! safety check to avoid division by zero in case of wrong density
-    #ifdef FFB_DENSITY_CHECKS
-        if (any(rho(:, :) <= 0.0_FP)) then
-            error stop "error: density is zero in collision/streaming step (rho_val <= 0)"
-        end if
-    #endif
+                ! read streamed distribution functions from f_next in all moving channels
+                ! (rest channel does not move)
+                f_1 = f(x, y, 1)
+                f_2 = f_next(x, y, 2)
+                f_3 = f_next(x, y, 3)
+                f_4 = f_next(x, y, 4)
+                f_5 = f_next(x, y, 5)
+                f_6 = f_next(x, y, 6)
+                f_7 = f_next(x, y, 7)
+                f_8 = f_next(x, y, 8)
+                f_9 = f_next(x, y, 9)
 
-        ! finalize and store density and velocity
-        u_x(:, :) = u_x(:, :) / rho(:, :)
-        u_y(:, :) = u_y(:, :) / rho(:, :)
-        u_squ(:, :) = u_x(:, :) * u_x(:, :) + u_y(:, :) * u_y(:, :)
+                f_pulled = [f_1, f_2, f_3, f_4, f_5, f_6, f_7, f_8, f_9]
 
-        ! collide and stream locally to destination channels
-        do i = 2, N_DIRS
+                rho_val = f_1 + f_2 + f_3 + f_4 + f_5 + f_6 + f_7 + f_8 + f_9
+                u_x_val = f_2 - f_4 + f_6 - f_7 - f_8 + f_9
+                u_y_val = f_3 - f_5 + f_6 + f_7 - f_8 - f_9
 
-            ! compute equilibrium distribution function for channel i
-            c_dot_u(:, :) = C_X_FP(i) * u_x(:, :) + C_Y_FP(i) * u_y(:, :)
-            f_eq(:, :) = W(i) * rho(:, :) * ( &
-                1.0_FP + &
-                3.0_FP * c_dot_u(:, :) + &
-                4.5_FP * c_dot_u(:, :) * c_dot_u(:, :) - &
-                1.5_FP * u_squ(:, :))
+                ! safety check to avoid division by zero in case of wrong density
+            #ifdef FFB_DENSITY_CHECKS
+                if (rho_val <= 0.0_FP) then
+                    error stop "error: density is zero in collision/streaming step (rho_val <= 0)"
+                end if
+            #endif
 
-            ! relax towards equilibrium and write to destination channel in this cell
-            f_next(:, :, i) = f_next(:, :, i) - omega * (f_next(:, :, i) - f_eq(:, :))
+                ! finalize and store density and velocity
+                u_x_val = u_x_val / rho_val
+                u_y_val = u_y_val / rho_val
+                u_squ = u_x_val * u_x_val + u_y_val * u_y_val
+                rho(x, y) = rho_val
+                u_x(x, y) = u_x_val
+                u_y(x, y) = u_y_val
+
+                ! collide and stream locally to destination channels
+                !DIR$ UNROLL(9)
+                do i = 1, N_DIRS
+
+                    ! compute equilibrium distribution function for channel i
+                    c_dot_u = C_X_FP(i) * u_x_val + C_Y_FP(i) * u_y_val
+                    f_eq_val = W(i) * rho_val * ( &
+                        1.0_FP + &
+                        3.0_FP * c_dot_u + &
+                        4.5_FP * c_dot_u * c_dot_u - &
+                        1.5_FP * u_squ)
+
+                    ! relax towards equilibrium and write to destination channel in this cell
+                    f_next_val = f_pulled(i) - omega * (f_pulled(i) - f_eq_val)
+                    f_next(x, y, i) = f_next_val
+                end do
+            end do
         end do
-
-        ! collide and stream for rest channel
-        f_eq(:, :) = W(1) * rho(:, :) * ( &
-            1.0_FP - &
-            1.5_FP * u_squ(:, :))
-        f_next(:, :, 1) = f(:, :, 1) - omega * (f(:, :, 1) - f_eq(:, :))
     end subroutine push_shift_streaming_collision_shear_wave
 
 
